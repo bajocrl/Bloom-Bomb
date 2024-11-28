79db4ed58f4401a87e39e81917510a2a4c9283b6
bajocrl <shgmkaze@gmail.com>
2024-11-28 16:42:26 +0800

    insert some credits on my behalf

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -95,6 +95,17 @@ function Board() {
             ))}
           </div>
         </div>
+        <p className='flex items-center gap-3'>
+          <span className='inline-flex items-center justify-center w-4 h-4 invert'>
+            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+              <path d='M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z' />
+            </svg>
+          </span>
+          <span className='text-lg opacity-80'>
+            {' '}
+            : <a href='https://x.com/caelmag'>@caelmag</a>
+          </span>
+        </p>
       </div>
     </div>
   );
